9ce7aea1dbfb73d7df0aea02c1feeecae55772cf
quiquelhappy <quiquelhappy@gmail.com>
2025-05-16 23:13:48 +0200

    dev

---
+++ dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build . --tag serverbench
+docker rm -f serverbench || true
+docker run \
+  --privileged \
+  --name serverbench \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp/containers:/containers \
+  -v /tmp/keys:/keys \
+  -e KEY="$1" \
+  -e HOSTNAME="$(hostname)" \
+  -e ENDPOINT="ws://localhost:3030" \
+  --pid=host \
+  --network=host \
+  serverbench
